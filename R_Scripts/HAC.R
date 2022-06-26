66a6b3d09d46a05b10ea76fa38ab6d25fc9a0a92
mkp131 <m.j.kipperman@gmail.com>
2022-06-26 21:20:27 +0100

    removed unused code

--- R_Scripts/HAC.R
+++ R_Scripts/HAC.R
@@ -5,30 +5,23 @@ library(HACSim)
 
 ## Bowmouth guitarfish COI
 
-N <- 200 # total number of sampled individuals (can be guess, doesn't need to be actual)
-Hstar <- 10 # total number of haplotypes
+N <- 200 # total number of sampled individuals (can be estimate, doesn't need to be actual)
+Hstar <- 10 # total number of haplotypes found in data set
 probs <- rep(1/Hstar, Hstar) # equal haplotype frequency distribution
   
 ## Simulate real species ## 
 # outputs file called "output.csv"
 HACSObj <- HACReal(filename = "output")
 
-## Simulate real species - subsampling ## 25% recovery (prop)
-#HACSObj <- HACReal(subsample = TRUE, prop = 0.25, conf.level = 0.95, 
-#                   p = 0.95, filename = "output", perms = 10000)
-
 ## Simulate real species - subsampling ## 95% recovery (prop)
-HACSObj <- HACReal(conf.level = 0.95, p = 0.95,
+# can rerun at p = 0.99 (for 99% recovery, p = 0.85% for 85% etc)
+HACSObj_bow <- HACReal(conf.level = 0.95, p = 0.95,
                    filename = "output", perms = 10000)
 
-## Simulate real species and all parameters changed - subsampling ##
-#HACSObj <- HACReal(perms = 10000, p = 0.90, subsample = TRUE, 
-#                   prop = 0.15, conf.level = 0.99, filename = "output")
-
 # user prompted to select appropriate FASTA file
-HAC.simrep(HACSObj)
+HAC.simrep(HACSObj_bow)
 
-## Simulate hypothetical species for Bowmouth guitarfish CR##
+##  Bowmouth guitarfish CR##
 
 N <- 200 # total number of sampled individuals
 Hstar <- 3 # total number of haplotypes
